10092f06ff26f49f85a3d788b8ce8811e30f1da9
Younes AJEDDIG <younes.ajeddig@live.fr>
2024-10-11 14:37:40 +0200

    Correction nom de package

--- setup.py
+++ setup.py
@@ -1,34 +1,31 @@
 from setuptools import setup, find_packages
 
-# Read the contents of README.md for long description
+# Lire le contenu de README.md pour la description longue
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sparse-pls',  # The name of your package
-    version='0.1.0',  # Start with an initial version
+    name='sparse_pls',  # Utilisez un underscore au lieu d'un tiret
+    version='0.1.1',  # Mettez à jour le numéro de version
     author='Younes AJEDDIG',
     author_email='younes.ajeddig@live.fr',
     description='Sparse Partial Least Squares (Sparse PLS) Regression',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yajeddig/SPARSE_PLS',  # Replace with your repository URL
-    packages=find_packages(),  # Automatically finds packages in the directory
+    url='https://github.com/yajeddig/SPARSE_PLS',  # Remplacez par l'URL de votre dépôt
+    packages=find_packages(),  # Trouve automatiquement les packages dans le répertoire
     install_requires=[
-        'numpy==1.26.4',
-        'pandas==2.2.2',
-        'scikit-learn==1.5.1',
-        'scipy==1.13.1',
-        'joblib==1.4.2'
+        'numpy>=1.20.0',
+        'pandas>=1.2.0',
+        'scikit-learn>=0.24.0',
+        'scipy>=1.6.0',
+        'joblib>=1.0.0'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',  # Minimum Python version requirement
-    package_data={
-        '': ['documentation/*.md'],  # Include documentation files
-    },
+    python_requires='>=3.7',  # Version minimale de Python requise
     include_package_data=True,
 )
